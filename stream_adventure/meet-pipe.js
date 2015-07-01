7b470fe1815d0fc17d683bfd6af1e52ab6d64b98
garthmiles <gmiles@denvergrp.com>
2015-07-01 16:17:47 -0400

    adding reference solution in comment block

--- stream_adventure/meet-pipe.js
+++ stream_adventure/meet-pipe.js
@@ -3,3 +3,10 @@ var fs = require('fs')
 
 // pipe contents to host
 fs.createReadStream(process.argv[2]).pipe(process.stdout)
+
+/*
+// reference solution
+var fs = require('fs')
+var file = process.argv[2]
+fs.createReadStream(file).pipe(process.stdout)
+*/
