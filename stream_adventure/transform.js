da22ec6cd74804b2105fa3fda3a9a2bb66bea5f5
garthmiles <gmiles@denvergrp.com>
2015-07-01 17:23:26 -0400

    adding abstraction details to write function

--- stream_adventure/transform.js
+++ stream_adventure/transform.js
@@ -4,7 +4,9 @@ var stream = through(write, end)
 
 // transform functions for buffers
 function write(buffer, encoding, next) {
-	// to be replaced with stuff
+	// produce output data and proceed to next chunk
+	this.push(buffer + '\n')
+	next()
 }
 
 function end(done) {
