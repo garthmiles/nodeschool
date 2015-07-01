d4446181ccc61d26eb752922302116e19946e6fa
garthmiles <gmiles@denvergrp.com>
2015-07-01 16:28:10 -0400

    simplified code

--- stream_adventure/input-output.js
+++ stream_adventure/input-output.js
@@ -1,10 +1,3 @@
-// load req'd modules 
-var fs = require('fs')
-
-// define variables
-var host = process.stdin
 
 // write to host
-fs.createReadStream(host).pipe(process.stdout)
-
-
+process.stdin.pipe(process.stdout)
