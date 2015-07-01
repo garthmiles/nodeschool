90569e4dd3c788b8dfa8ed8b4a2e94647afb9c83
garthmiles <gmiles@denvergrp.com>
2015-07-01 16:51:06 -0400

    adding function placeholders

--- stream_adventure/transform.js
+++ stream_adventure/transform.js
@@ -1,3 +1,14 @@
 // load & set req'd modules
 var through = require('through2') 
 var stream = through(write, end) 
+
+// transform functions for buffers
+function write(buffer, encoding, next) {
+	// to be replaced with stuff
+}
+
+function end(done) {
+	// to be replaced with stuff
+}
+
+
