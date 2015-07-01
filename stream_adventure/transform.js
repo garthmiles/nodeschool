9bba62f3ab31a4d6ec16f09549b779271531d61e
garthmiles <gmiles@denvergrp.com>
2015-07-01 17:40:43 -0400

    converting buffer to string

--- stream_adventure/transform.js
+++ stream_adventure/transform.js
@@ -5,12 +5,15 @@ var stream = through(write, end)
 // transform functions for buffers
 function write(buffer, encoding, next) {
 	// produce output data and proceed to next chunk
-	this.push(buffer + '\n')
+	this.push(buffer.toString() + '\n')
 	next()
 }
 
 function end(done) {
 	// to be replaced with stuff
+	done()
 }
+
+process.stdin.pipe(write).pipe(process.stdout)
 
 
