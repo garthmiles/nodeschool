4ba544a31a5699858af6c6bd68818d284f88204c
garthmiles <gmiles@denvergrp.com>
2015-07-04 16:16:33 -0400

    convert to uppercase

--- stream_adventure/transform.js
+++ stream_adventure/transform.js
@@ -4,7 +4,7 @@ var thru = require('through2')
 // transform functions for buffers
 var tStream = thru(function (bffr, enc, next) {
 	// produce output data and proceed to next chunk
-	this.push(bffr.toString())
+	this.push(bffr.toString().toUpperCase())
 	next()
 })
 
